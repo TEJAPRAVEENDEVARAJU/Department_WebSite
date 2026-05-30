44096bd7e84b6e800ea356281703cb5e345073a2
OBULASETTY TEJA PRAVEEN DEVA RAJU <120026630+TEJAPRAVEENDEVARAJU@users.noreply.github.com>
2026-05-30 16:11:12 +0530

    Update Admin_board.php

--- dept/Admin_board.php
+++ dept/Admin_board.php
@@ -9,10 +9,10 @@ if (!isset($_SESSION['employee_name'])) {
 }
 
 // ✅ Database Connection
-$host = 'sql211.infinityfree.com';
-$user = 'if0_39030192';
-$password = 'sUheL4DCaoSn';
-$dbname = 'if0_39030192_infomatrix';
+$host = 'host';
+$user = 'user';
+$password = 'password';
+$dbname = 'dbname';
 $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
